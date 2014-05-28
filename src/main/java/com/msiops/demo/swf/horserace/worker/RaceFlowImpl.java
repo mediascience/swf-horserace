ee8355f43fe0ebdf2e54b7f4ac5f2dd66e564044
Greg Wiley <aztec.rex@jammm.com>
2014-05-27 22:13:55 -0700

    [license] license header in sources

--- src/main/java/com/msiops/demo/swf/horserace/worker/RaceFlowImpl.java
+++ src/main/java/com/msiops/demo/swf/horserace/worker/RaceFlowImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to Media Science International (MSI) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. MSI
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.msiops.demo.swf.horserace.worker;
 
 import java.util.ArrayList;
@@ -9,6 +25,12 @@ import com.amazonaws.services.simpleworkflow.flow.annotations.ExponentialRetry;
 import com.amazonaws.services.simpleworkflow.flow.annotations.Wait;
 import com.amazonaws.services.simpleworkflow.flow.core.Promise;
 
+/**
+ * Race flow implemtation.
+ * 
+ * @author greg wiley <aztec.rex@jammm.com>
+ *
+ */
 public class RaceFlowImpl implements RaceFlow {
 
 	private final RaceActivitiesClient race = new RaceActivitiesClientImpl();
